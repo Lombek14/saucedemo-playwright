a89594320c2eb9320ee618056cee65ab41ae43ca
Mahoula <mahouladosso@pm.me>
2025-11-26 04:20:01 -0600

    perf: add soak test (20 VUs, 3m) to evaluate API long-term stability

---
+++ k6-tests/reqres-soak.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 20,          // 20 virtual users
+  duration: '3m', // run test for 15 seconds
+};
+
+export default function () {
+  const response = http.get('https://reqres.in/api/users/2', {
+    headers: {
+      'x-api-key': 'reqres-free-v1',
+    },
+  });
+
+  check(response, {
+    'status is 200': (resp) => resp.status === 200,
+    'response time < 500ms': (resp) => resp.timings.duration < 500,
+  });
+
+  sleep(1);
+}
